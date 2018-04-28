51108606a7bfc90b4c29d8767f2d155562bf901d
Gian Lorenzo Meocci <glmeocci@gmail.com>
2018-04-28 17:17:23 +0200

    fix formatting

--- chain/lib/chain.ex
+++ chain/lib/chain.ex
@@ -1,25 +1,25 @@
 defmodule Chain do
-  defstruct (
+  defstruct(
     next_node: nil,
-    count:     4
+    count: 4
   )
 
   def start_link(next_node) do
-    spawn_link(Chain, :message_loop, %Chain{ next_node: next_node })
+    spawn_link(Chain, :message_loop, %Chain{next_node: next_node})
     |> Process.register(:chainer)
   end
 
-  def message_loop(%Chain{ count: 0}) do
-    IO.puts "done"
+  def message_loop(%Chain{count: 0}) do
+    IO.puts("done")
   end
 
   def message_loop(state = %Chain{}) do
     receive do
       {:trigger, list} ->
         IO.inspect(list)
-        send({:chainer, state.next_node}, {:trigger, [ node() | list ]})
+        send({:chainer, state.next_node}, {:trigger, [node() | list]})
     end
-    message_loop(%Chain{ state | count: state.count - 1 })
-  end
 
+    message_loop(%Chain{state | count: state.count - 1})
+  end
 end
